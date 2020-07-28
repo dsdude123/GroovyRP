38715ae114372b69733006065382f87ad56f92c9
Joel Parkinson <joelparkinson@live.co.uk>
2020-07-28 15:50:08 +0100

    Big Cleanup
    Code & logic cleanup.
    
    Changes:
    * Created a TrackInfo class to cleanup setting the rich pressense.
    * Moved the info fetching code into a class that handles it to cleanup Program.cs.
    * Renamed the methods to make them more meaningful when read.
    * Update all project dependencies.
    * Moved to _client.SetPressence in place of setting details and state using the respective Update* methods.
    * Applied a large and small image asset.
    * Moved the console output into the PressenceUpdated event.
    * Ensured errors from the RPCCLient are reported to the console.
    * Moved several pieces of logic out of the main loop.
    * Various other changes to clean up Program.cs.
    
    Bugs Fixes:
    * The client would stop working due to incorrect logic when the track was paused and then played again. This is not an issue anymore.
    * The client would constantly clear and repost message to the console when recovering from certain other issues. This is now fixed as we check the state before updating the console.

---
+++ GroovyRP/GrooveInfoFetcher.cs
@@ -0,0 +1,132 @@
+﻿using CSCore.CoreAudioAPI;
+using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace GroovyRP
+{
+    public class GrooveInfoFetcher
+    {
+        public TrackInfo GetTrackInfo()
+        {
+            var result = new TrackInfo();
+
+            Process handleFinder = Process.Start("openedfilesview\\OpenedFilesView.exe", "/processfilter Music.UI.exe /scomma files.csv");
+            handleFinder.WaitForExit();
+            StreamReader streamReader = System.IO.File.OpenText("files.csv");
+            CsvParser csvParser = new CsvParser(streamReader, CultureInfo.InvariantCulture);
+
+            string[] row = csvParser.Read();
+            List<string[]> rows = new List<string[]>();
+
+            while (row != null)
+            {
+                rows.Add(row);
+                row = csvParser.Read();
+            }
+
+            foreach (string[] data in rows)
+            {
+                try
+                {
+                    if (supportedFileTypes.Contains(data[20], StringComparer.OrdinalIgnoreCase))
+                    {
+                        var media = TagLib.File.Create(data[1]);
+                        result.Title = media.Tag.Title;
+                        if (media.Tag.Performers.Length > 0)
+                        {
+                            result.Artist = media.Tag.Performers.First();
+                        }
+                        else
+                        {
+                            if (media.Tag.AlbumArtists.Length > 0)
+                            {
+                                result.Artist = media.Tag.AlbumArtists.First();
+                            }
+                        }
+                        result.Album = media.Tag.Album;
+                        break;
+                    }
+                    streamReader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"{ex.Message} \n{ex.StackTrace}");
+                    return result;
+                }
+            }
+            return result;
+        }
+
+        public bool IsUsingAudio()
+        {
+            Process[] grooveMusics = Process.GetProcessesByName("Music.UI");
+            if (grooveMusics.Length > 0)
+            {
+                AudioSessionManager2 sessionManager;
+                using (var enumerator = new MMDeviceEnumerator())
+                {
+                    using (var device = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia))
+                    {
+                        sessionManager = AudioSessionManager2.FromMMDevice(device);
+                    }
+                }
+
+                using (var sessionEnumerator = sessionManager.GetSessionEnumerator())
+                {
+                    foreach (var session in sessionEnumerator)
+                    {
+                        bool targetProcess = false;
+                        using (var sessionControl = session.QueryInterface<AudioSessionControl2>())
+                        {
+                            var process = sessionControl.Process;
+                            if (process.ProcessName.Equals("Music.UI"))
+                            {
+                                targetProcess = true;
+                            }
+                        }
+                        using (var audioMeter = session.QueryInterface<AudioMeterInformation>())
+                        {
+                            if (audioMeter.GetPeakValue() > 0 && targetProcess)
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+
+                return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static readonly string[] supportedFileTypes = {
+            "aa",
+            "aax",
+            "aac",
+            "aiff",
+            "ape",
+            "dsf",
+            "flac",
+            "m4a",
+            "m4b",
+            "m4p",
+            "mp3",
+            "mpc",
+            "mpp",
+            "ogg",
+            "oga",
+            "wav",
+            "wma",
+            "wv",
+            "webm"
+        };
+    }
+}
