1091f2155987f61cf58291e85057ee6d3a8a3797
Conner Starkel <conner@starkel.us>
2021-02-01 20:01:59 -0800

    Fix console output encoding

--- GroovyRPCore/Program.cs
+++ GroovyRPCore/Program.cs
@@ -14,6 +14,7 @@ namespace GroovyRP
 
         private static void Main()
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.WriteLine(appDetails);
             Console.WriteLine("\nNothing Playing");
 
